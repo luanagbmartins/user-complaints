660360c890ddf36c637ac7edf4d26c1920b3835b
luanagbmartins <luanagbmartins@gmail.com>
2020-03-31 13:25:40 -0300

    rename folder assets to static

--- app/app.py
+++ app/app.py
@@ -3,12 +3,16 @@ from flask_restful import Resource, Api
 import os
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-class Index(Resource):
-    def get(self):
-        return render_template('index.html')
+# class Index(Resource):
+#     def get(self):
+#         return render_template('index.html')
 
-api.add_resource(Index, '/')
+# api.add_resource(Index, '/')
+
+@app.route('/')
+def index():
+      return render_template('index.html')
