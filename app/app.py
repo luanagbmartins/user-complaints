c8b9e5cdbc7a560e0ab4db7be88c2fc0f54634b3
luanagbmartins <luanagbmartins@gmail.com>
2020-04-01 22:55:43 -0300

    update frontend

--- app/app.py
+++ app/app.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 import os
 
 app = Flask(__name__)
-# api = Api(app)
-
-app.config.from_object(os.environ['APP_SETTINGS'])
-
-# class Index(Resource):
-#     def get(self):
-#         return render_template('index.html')
-
-# api.add_resource(Index, '/')
-
 @app.route('/')
-def index():
+def my_form():
       return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+      # prediction, confidence = fp.process_text(complaint)
+      prediction = 'Credict card'
+      confidence = '0.98'
+      name = request.form['name']
+      return render_template('prediction.html', name = name, result = prediction, confidence = confidence)
