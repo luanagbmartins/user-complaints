db097f99417969aff533db77c7b1d9d5271f1c21
luanagbmartins <luanagbmartins@gmail.com>
2020-04-21 23:33:26 -0300

    add chosen method save folder

--- src/features/feature_extraction.py
+++ src/features/feature_extraction.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import luigi
 import pickle
@@ -13,7 +14,8 @@ from data import PreprocessData
 
 
 class FeatureExtraction(luigi.Task):
-
+    # TODO parameters description with 'help' argument
+    method = luigi.ChoiceParameter(choices = ['tfidf', 'word2vec'], default = 'tfidf')
     lowercase = luigi.BoolParameter()
     tokenizer = luigi.OptionalParameter(default = None)
     stop_words = luigi.OptionalParameter(default = None)
@@ -23,7 +25,7 @@ class FeatureExtraction(luigi.Task):
     max_features = luigi.OptionalParameter(default = None)
     vocabulary = luigi.OptionalParameter(default = None)
     binary = luigi.BoolParameter()
-    norm = luigi.ChoiceParameter(choices = ['l1', 'l2', 'none'], default = 'l2')
+    norm = luigi.ChoiceParameter(choices = ['l1', 'l2', 'None'], default = 'l2')
     use_idf = luigi.BoolParameter()
     smooth_idf = luigi.BoolParameter()
     sublinear_tf = luigi.BoolParameter()
@@ -31,36 +33,43 @@ class FeatureExtraction(luigi.Task):
     def __init__(self, *args, **kwargs):
         super(FeatureExtraction, self).__init__(*args, **kwargs)
 
-        if self.norm == 'none':
-            self.norm = None
-
+        if self.norm == 'None':
+            self.norm = ast.literal_eval(self.norm)
         self.max_df = ast.literal_eval(self.max_df)
         self.min_df = ast.literal_eval(self.min_df)
+
+        save_folder = os.path.join('data/processed', self.method)
+        if not os.path.exists(save_folder):
+            os.makedirs(save_folder)
+        self.save_folder = save_folder
 
     def requires(self):
         return PreprocessData()
     
     def output(self):
-        return { 'features': luigi.LocalTarget('data/processed/features.pickle'),
-                 'labels': luigi.LocalTarget('data/processed/labels.pickle') }
+        return { 'features': luigi.LocalTarget(os.path.join(self.save_folder, 'features.pickle')),
+                 'labels': luigi.LocalTarget(os.path.join(self.save_folder, 'labels.pickle')) }
 
     def run(self):
         print('---> Extracting features...')
         data = pd.read_pickle(self.input().path)
         features, labels = self.feature_extraction(data)
 
-        pickle.dump(features, open('data/processed/features.pickle', 'wb'))
-        pickle.dump(labels, open('data/processed/labels.pickle', 'wb'))
+        pickle.dump(features, open(os.path.join(self.save_folder, 'features.pickle'), 'wb'))
+        pickle.dump(labels, open(os.path.join(self.save_folder, 'labels.pickle'), 'wb'))
         
         print('Done!')
 
     def feature_extraction(self, data):
+        if self.method == 'tfidf':
+            return self.tfidf(data)
+        elif self.method == 'word2vec':
+            return self.word2vec(data)
+
+    def tfidf(self, data):
         print('---> Converting a collection of raw documents to a matrix of TF-IDF features...')
 
-        products_count_df = data.groupby('product_id').complaint.count().to_frame()
-        products_count_df.reset_index(level=0, inplace=True)
-        labels = np.array(products_count_df['product_id'].unique())
-
+        labels = np.array(data['product_id'])
         data['complaints_untokenized'] = data['complaint'].apply(lambda x: ' '.join(x))
         tfidf_converter = TfidfVectorizer( lowercase = self.lowercase, 
                                            tokenizer = self.tokenizer,
@@ -75,7 +84,12 @@ class FeatureExtraction(luigi.Task):
                                            use_idf = self.use_idf,
                                            smooth_idf = self.smooth_idf,
                                            sublinear_tf = self.sublinear_tf )
+
         features = tfidf_converter.fit_transform(data.complaints_untokenized)
         print('---> Each of the %d complaints is represented by %d features (TF-IDF score of unigrams and bigrams)' %(features.shape))
 
         return features, labels
+
+        def word2vec(self, data):
+            # TODO 
+            pass
