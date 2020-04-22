bc994c185b8f8939dbe0f22d116cca11c9224f75
luanagbmartins <luanagbmartins@gmail.com>
2020-04-22 10:50:48 -0300

    add fetch prediction service

--- app/app.py
+++ app/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, render_template
-from flask_restful import Resource, Api
-import os
+from services import fetch_prediction as fp
 
 app = Flask(__name__)
 @app.route('/')
-def my_form():
+def index():
       return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def my_form_post():
-      # prediction, confidence = fp.process_text(complaint)
-      prediction = 'Credict card'
-      confidence = '0.98'
+def prediction():
+      complaint = request.form['complaint']
       name = request.form['name']
-      return render_template('prediction.html', name = name, result = prediction, confidence = confidence)
+
+      main_product, sub_product = fp.process_text(complaint)
+
+      return render_template('prediction.html', name = name, main_product = main_product, sub_product = sub_product)
